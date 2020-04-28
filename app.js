e1fd14995424123f881173a4ca79aa2ecabfc9a3
Mahfuz Lafin <64304899+mahfuzlafin@users.noreply.github.com>
2020-04-29 00:39:32 +0600

    updating random button

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const h3 = document.getElementById('h3'),
       leftInput = document.getElementById('leftColorInput'),
       rightInput = document.getElementById('rightColorInput'),
-      body = document.body;
+      body = document.body,
+      randomButton = document.getElementById('randomButton');
 
 
 function setGradient(){
     body.style.background = `linear-gradient(to right, ${leftColorInput.value}, ${rightColorInput.value})`;
-    h3.textContent = `${body.style.background};`
+    h3.textContent = `${body.style.background};`;
 }
 
 
 leftColorInput.addEventListener('input', setGradient);
 rightColorInput.addEventListener('input', setGradient);
+
+
+randomButton.addEventListener('click', function(){
+    const randomColor1 = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    const randomColor2 = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    body.style.background = `linear-gradient(to right, ${randomColor1}, ${randomColor2})`;
+    h3.textContent = `${body.style.background};`;
+});
